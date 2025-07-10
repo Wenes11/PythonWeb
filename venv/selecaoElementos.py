84368b5fe582bb5a0aad5e4502f5a63a69fe1b35
Wenes11 <wenes.vargas10@gmail.com>
2025-07-10 20:17:09 -0300

    Pegando tabela do globo esporte

---
+++ venv/selecaoElementos.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+# Inicializa o driver
+driver = webdriver.Chrome()
+
+# Acessa o site do Brasileirão
+driver.get("https://ge.globo.com/futebol/brasileirao-serie-a/")
+
+# Aguarda carregamento da página (por segurança)
+
+# Tenta encontrar o elemento com ID 'class'
+table = driver.find_element(By.CLASS_NAME,'tabela__futebol')
+print(table.text)
+# enta encontrar o elemento com ID name
+# driver.find_element(By.NAME, '').get_property('')
+
+# Verifica se encontrou algum elemento
+
+# Espera 100 segundos antes de fechar
+time.sleep(100)
