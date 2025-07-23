2ddf81b345e9e63f3f1c1eb7420de5c3ee908386
João Vitor <wenes.vargas10@gmail.com>
2025-07-22 23:06:57 -0300

    Nova forma de printar e coletar dados table Bsccin

--- venv/selecaoElementos.py
+++ venv/selecaoElementos.py
@@ -11,7 +11,7 @@ driver.get("https://ge.globo.com/futebol/brasileirao-serie-a/")
 # Aguarda carregamento da página (por segurança)
 
 # Tenta encontrar o elemento com ID 'class'
-table = driver.find_element(By.CLASS_NAME,'tabela__futebol')
+table = driver.find_element(By.CLASS_NAME,'tabela tabela__pontos-corridos')
 print(table.text)
 # enta encontrar o elemento com ID name
 # driver.find_element(By.NAME, '').get_property('')
--- venv/xpatch.py
+++ venv/xpatch.py
@@ -5,6 +5,12 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 
 driver.get("https://ge.globo.com/futebol/brasileirao-serie-a/")
-driver.find_elements(By.XPATH,'//div[@id="menu-curtain"]')
+table = driver.find_element(By.XPATH,'/html/body/div[2]/main/div[2]/div/section[1]/article/section[1]/div/table[1]')
+table_lines = table.find_elements(By.CLASS_NAME,'classificacao__tabela--linha')
+clubs = []
 
-print(driver)
+for line in table_lines:
+    club_name = line.find_element(By.CLASS_NAME, 'classificacao__equipes.classificacao__equipes--nome').get_attribute('innerHTML')
+    clubs.append(club_name)
+
+print(clubs)
